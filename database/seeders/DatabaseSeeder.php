4c01af827f50e362b6ad46c53d3ccee117cb2405
MUHAMMAD IHSAN <ihsannms28@gmail.com>
2026-01-15 14:32:31 +0700

    Register AdminUserSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PackageSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }
